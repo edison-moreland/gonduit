2295896dc234ad2a93b1e6a617cea6e3ce3f034d
Edison Moreland <edison@devdude.net>
2019-08-26 13:05:57 -0700

    Combined auth routes to single file

--- api/routes/auth/routes.go
+++ api/routes/auth/routes.go
@@ -1,7 +1,11 @@
 package auth
 
 import (
+	"encoding/json"
+	"github.com/edison-moreland/gonduit/api/helpers"
+	"github.com/edison-moreland/gonduit/authentication"
 	"github.com/edison-moreland/gonduit/authentication/jwt"
+	"github.com/edison-moreland/gonduit/models"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -11,4 +15,82 @@ func AddRoutes(router *mux.Router) {
 	router.Path("/users").Methods(http.MethodPost).HandlerFunc(register).Name("register")
 	router.Path("/user").Methods(http.MethodGet).Handler(jwt.JWTRequired(currentUser)).Name("currentuser")
 	router.Path("/user").Methods(http.MethodPut).Handler(jwt.JWTRequired(updateUser)).Name("updateuser")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	request := userRequest{User: userRequestBody{}}
+	err := helpers.UnmarshalRequestBody(r.Body, &request)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	user, err := authentication.Login(request.User.Username, request.User.Password)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(userResponse{User: user}); err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	request := userRequest{User: userRequestBody{}}
+	err := helpers.UnmarshalRequestBody(r.Body, &request)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	// Make sure email field is filled (its optional on struct)
+	if request.User.Email == "" {
+		helpers.Err422("Email field can't be blank", w)
+		return
+	}
+
+	// Create user
+	newUser := models.User{Username: request.User.Username, Email: request.User.Email}
+	err = newUser.UpdatePassword(request.User.Password)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+	newUser.Save()
+
+	// Login
+	user, err := authentication.Login(request.User.Username, request.User.Password)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(userResponse{User: user}); err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+}
+
+func currentUser(w http.ResponseWriter, r *http.Request) {
+	// Get current user from context
+	user := r.Context().Value("user").(models.User)
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(userResponse{User: user}); err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	// Get current user from context
+	user := r.Context().Value("user").(models.User)
 }
