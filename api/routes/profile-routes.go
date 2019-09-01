62448021111bb5d45cdd692d5cdfd5f29d7d3557
Edison-Github <edison@devdude.net>
2019-09-01 10:22:39 -0700

    Created profile routes

---
+++ api/routes/profile-routes.go
@@ -0,0 +1,108 @@
+package routes
+
+import (
+	"encoding/json"
+	"github.com/edison-moreland/gonduit/api/helpers"
+	"github.com/edison-moreland/gonduit/authentication/jwt"
+	"github.com/edison-moreland/gonduit/models"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func AddProfileRoutes(router *mux.Router) {
+	router.Path("/profile/{username}").Methods(http.MethodGet).Handler(jwt.Optional(getProfile)).Name("getprofile")
+	router.Path("/profile/{username}/follow").Methods(http.MethodPost).Handler(jwt.Required(followUser)).Name("followUser")
+	router.Path("/profile/{username}/follow").Methods(http.MethodDelete).Handler(jwt.Required(unfollowUser)).Name("unfollowUser")
+}
+
+type profileResponse struct {
+	Profile models.Profile `json:"profile"`
+}
+
+func getProfile(w http.ResponseWriter, r *http.Request) {
+	// Get username from url
+	username := mux.Vars(r)["username"]
+
+	// Find user
+	profileUser, err := models.GetUser(username)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	// If user is logged in, find out if they're following this profile
+	loggedInUser := r.Context().Value("user").(models.User)
+	following := false
+	if loggedInUser.Username != "" {
+		following = loggedInUser.IsFollowingUser(username)
+	}
+
+	// Generate profile
+	profile := profileUser.GetProfile(following)
+
+	// Write it out
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(profileResponse{Profile: profile}); err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+}
+
+func followUser(w http.ResponseWriter, r *http.Request) {
+	// Get username from url
+	username := mux.Vars(r)["username"]
+
+	// Find user
+	profileUser, err := models.GetUser(username)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	// Follow user
+	loggedInUser := r.Context().Value("user").(models.User)
+	err = loggedInUser.FollowUser(username)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+	loggedInUser.Save()
+
+	// Generate profile
+	profile := profileUser.GetProfile(loggedInUser.IsFollowingUser(username))
+
+	// Write it out
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(profileResponse{Profile: profile}); err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+}
+
+func unfollowUser(w http.ResponseWriter, r *http.Request) {
+	// Find user with username from url
+	profileUser, err := models.GetUser(mux.Vars(r)["username"])
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+
+	// Follow user
+	loggedInUser := r.Context().Value("user").(models.User)
+	err = loggedInUser.UnFollowUser(profileUser.Username)
+	if err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+	loggedInUser.Save()
+
+	// Generate profile
+	profile := profileUser.GetProfile(loggedInUser.IsFollowingUser(profileUser.Username))
+
+	// Write it out
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(profileResponse{Profile: profile}); err != nil {
+		helpers.Err422(err.Error(), w)
+		return
+	}
+}
